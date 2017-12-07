a400d071bd5e15a963538c6e519792a1ca1d7e01
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-12-07 20:14:44 +0100

    add ignore raycast to ferris rides

--- SeniorProjects3/Assets/Scripts/BecomeChild.cs
+++ SeniorProjects3/Assets/Scripts/BecomeChild.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using VRTK;
 
 public class BecomeChild : MonoBehaviour {
-    protected int FERRISLAYER = 8;
     protected Transform originalParent;
 	public GameObject playArea;
     //protected FixedJoint joint;
