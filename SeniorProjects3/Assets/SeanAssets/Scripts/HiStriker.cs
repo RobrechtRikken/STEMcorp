ca7c9efb07954b6b8b9494bfeafb17689c4cbf2f
Unknown <sean.tempels@student.kdg.be>
2017-12-07 13:47:53 +0100

    Sean Pushed
    Added HiStriker
    Added Hammer
    Added HiStriker script

---
+++ SeniorProjects3/Assets/SeanAssets/Scripts/HiStriker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiStriker : MonoBehaviour {
+  
+  private Rigidbody rb;
+  public GameObject weight;
+  private Vector3 rbForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void OnCollisionEnter (Collision col)
+  {
+    if (col.gameObject.name == "Hammer")
+    {
+			rb = col.gameObject.GetComponent<Rigidbody>();
+			weight.GetComponent<Rigidbody>().AddForce(new Vector3(0, rb.velocity.y,0));
+    }
+  }
+}
